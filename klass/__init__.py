aec998e6656c88a7c19fe61bfb40747a8130ffa4
Carl Corneil <cfc@ssb.no>
2023-03-28 09:41:25 +0200

    working on init

---
+++ klass/__init__.py
@@ -0,0 +1,7 @@
+__version__ = "0.0.1"
+
+__all__ = []
+
+from .klass_requests import classifications, classification_search, classification_by_id, codes, codes_at, version_by_id, variant, variant_at, variant_by_id, corresponds, corresponds_at, correspondance_table_by_id, changes, classificationfamilies, classificationfamilies_by_id
+
+print(locals())
