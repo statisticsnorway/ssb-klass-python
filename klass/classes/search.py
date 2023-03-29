db98fc065fa1b58fd39fc65e45035ab9b41eee0d
Carl Corneil <cfc@ssb.no>
2023-03-29 13:14:20 +0200

    family search

--- klass/classes/search.py
+++ klass/classes/search.py
@@ -8,5 +8,24 @@ class KlassSearchClassifications():
     pass
 
 class KlassSearchClassificationFamilies():
-    pass
-
+    def __init__(self,
+                 ssbsection: str = "",
+                 include_codelists: bool = False,
+                 language: str = "nb",):
+        result = classificationfamilies(ssbsection=ssbsection,
+                                             include_codelists=include_codelists,
+                                             language=language,)
+        self.families = result["_embedded"]["classificationFamilies"]
+        self.links = result["_links"]
+        families_replace = []
+        for fam in self.families:
+            fam["family_id"] = int(fam["_links"]["self"]["href"].split("/")[-1])
+            families_replace.append(fam)
+        self.families = families_replace
+        
+    def __str__(self):
+        
+        result = ""
+        for fam in self.families:
+            result += f"{fam['family_id']} - {fam['name']} - Number of classifications: {fam['numberOfClassifications']}\n"
+        return result
