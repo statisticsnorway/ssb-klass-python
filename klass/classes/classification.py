592c89593e13b5a9f44da9bba513898de63459c7
Carl Corneil <cfc@ssb.no>
2023-03-29 12:49:28 +0200

    classification str get version

--- klass/classes/classification.py
+++ klass/classes/classification.py
@@ -1,11 +1,40 @@
 from ..requests.klass_requests import classification_by_id
-
+from .version import KlassVersion
 
 class KlassClassification:
-    def __init__(self, classification_id: str):
-        for key, value in classification_by_id(classification_id).items():
-            setattr(self, key, value)
 
-    def get_version(version_name: str = "", version_id: str = ""):
-        if version_name:
-            return KlassVersion(version_name=version_name)
+
+    def __init__(self, 
+                 classification_id: str,
+                 language: str = "nb",
+                 include_future: bool = False):
+        self.classification_id = classification_id
+        for key, value in classification_by_id(classification_id,
+                                               language=language,
+                                               include_future=include_future).items():
+            setattr(self, key, value)
+        version_replace = []
+        for ver in self.versions:
+            ver["version_id"] = int(ver["_links"]["self"]["href"].split("/")[-1])
+            version_replace.append(ver)
+        self.versions = version_replace
+
+
+    def __str__(self):
+        contact = "Contact Person:\n"
+        for k, v in self.contactPerson.items():
+            contact += f"\t{k}: {v}\n"
+        units = ", ".join(self.statisticalUnits)
+        result = f"""Classification {self.classification_id}: {self.name}
+        Owning Section: {self.owningSection}
+        {contact}
+        Statistical Units: {units}
+        Number of versions: {len(self.versions)}
+        
+{self.description}
+        """
+        return result
+    
+    @staticmethod
+    def get_version(version_id):
+        return KlassVersion(version_id)
