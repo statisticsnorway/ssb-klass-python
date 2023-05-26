ed7a935be181a8832af0d6c69dfe5b2cca662c07
Carl Corneil <cfc@ssb.no>
2023-05-26 21:11:33 +0200

    new actual test, is failing yay

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,12 +2,22 @@ from unittest import mock
 
 import pytest
 
-from klass import KlassClassification
-from tests.mock_request_functions import classification_by_id_success
+import klass
+from tests.mock_request_functions import (
+    classification_by_id_success,
+    classification_search_success,
+)
 
 
 @pytest.fixture
 @mock.patch("klass.classes.classification.classification_by_id")
 def klass_classification_success(test_classification_by_id):
     test_classification_by_id.return_value = classification_by_id_success()
-    return KlassClassification("0")
+    return klass.KlassClassification("0")
+
+
+@pytest.fixture
+@mock.patch("klass.classes.search.classification_search")
+def klass_classification_search_success(test_classification_search):
+    test_classification_search.return_value = classification_search_success()
+    return klass.KlassSearchClassification("Nus")
