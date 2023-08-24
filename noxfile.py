db9dd2b43a6d2f2e7ef7216806612766cf9cacf4
Carl Corneil <cfc@ssb.no>
2023-08-24 12:17:12 +0200

    remove include to check

--- noxfile.py
+++ noxfile.py
@@ -6,10 +6,4 @@ def run_coverage(session, cov_fail: int = 23):
     session.install(".")
     session.install("coverage[toml]")
 
-    session.run(
-        "coverage",
-        "report",
-        "--include=klass/*",
-        "--omit=*__init__.py",
-        f"--fail-under={cov_fail}",
-    )
+    session.run("coverage", "report", "--omit=*__init__.py", f"--fail-under={cov_fail}")
