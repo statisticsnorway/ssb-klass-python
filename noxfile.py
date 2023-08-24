391a49e4981e99e5372558e963317a8fb5d6adbf
Carl Corneil <cfc@ssb.no>
2023-08-24 11:39:59 +0200

    change name of decorated function and action job name

--- noxfile.py
+++ noxfile.py
@@ -2,7 +2,7 @@ from nox_poetry import session
 
 
 @session(python=["3.8", "3.9", "3.10", "3.11"])
-def run_pytest_klass_folder(session, cov_fail: int = 23):
+def run_coverage(session, cov_fail: int = 23):
     session.install(".")
     session.install("coverage[toml]")
     session.run(
