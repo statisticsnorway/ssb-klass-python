9fb06b23666e6cb3851d784482c53a73f1ba3be5
Carl Corneil <cfc@ssb.no>
2023-08-28 08:44:25 +0200

    regressing to just using pytest with matrix

--- noxfile.py
+++
@@ -1,9 +0,0 @@
-from nox_poetry import session
-
-
-@session()
-def run_coverage(session, cov_fail: int = 23):
-    session.install(".")
-    session.install("coverage[toml]")
-
-    session.run("coverage", "report", f"--fail-under={cov_fail}")
