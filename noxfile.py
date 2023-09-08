1d788edeac7c4ee833c2d3ed28ec245732006591
Arne Sørli <81353974+arneso-ssb@users.noreply.github.com>
2023-09-08 11:21:30 +0200

    Make nox run tests and report coverage

--- noxfile.py
+++ noxfile.py
@@ -4,6 +4,7 @@ from nox_poetry import session
 @session()
 def run_coverage(session, cov_fail: int = 23):
     session.install(".")
-    session.install("coverage[toml]")
+    session.install("coverage[toml]", "pytest")
 
-    session.run("coverage", "report", f"--fail-under={cov_fail}")
+    session.run("coverage", "run", "-m", "pytest")
+    session.run("coverage", "report", "--skip-empty", f"--fail-under={cov_fail}")
