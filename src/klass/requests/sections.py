e7aeae93fa57cf629cee5e55890599bf1f696eab
Carl F. Corneil <aecorn@gmail.com>
2023-12-01 08:53:24 +0100

    pre-commit fixes sections.py

--- src/klass/requests/sections.py
+++ src/klass/requests/sections.py
@@ -1,7 +1,8 @@
 from functools import lru_cache
-from ..klass_config import KlassConfig
+
 import requests
 
+from ..klass_config import KlassConfig
 
 # As these functions are used by the validate functions also,
 # they are in their own file to avoid circular imports
@@ -9,11 +10,13 @@ import requests
 
 @lru_cache(maxsize=1)
 def sections_list() -> list:
-    url = KlassConfig().BASE_URL + '/ssbsections'
+    """Gets the sections that are registered in KLASS-api. Unlikely to change often, so we cache this."""
+    url = KlassConfig().BASE_URL + "/ssbsections"
     response = requests.get(url).json()
     sections = [x["name"] for x in response["_embedded"]["ssbSections"]]
     return sections
 
 
 def sections_dict() -> dict:
+    """Reshapes the section list to a dict, with section-numbers as keys."""
     return {s.split(" ")[0]: s for s in sections_list()}
