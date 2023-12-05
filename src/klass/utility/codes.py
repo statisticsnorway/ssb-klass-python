ae160a26fe4c6613796f971224aea43cf3ef4870
Carl F. Corneil <aecorn@gmail.com>
2023-12-05 14:44:55 +0100

    imperative mood my ass

--- src/klass/utility/codes.py
+++ src/klass/utility/codes.py
@@ -6,7 +6,7 @@ from ..classes.codes import KlassCodes
 def get_codes(
     classification_id: str = "", date: str = "", dataframe: bool = False
 ) -> KlassCodes | pd.DataFrame:
-    """Gets the codelist-data from a classification as a dataframe, or a KlassCodes-wrapper (includes metadata)."""
+    """Get the codelist-data from a classification as a dataframe, or a KlassCodes-wrapper (includes metadata)."""
     if dataframe:
         return KlassCodes(classification_id, date).data
     return KlassCodes(classification_id, date)
