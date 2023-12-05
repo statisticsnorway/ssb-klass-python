e53bb743a1c78e1b2ade961626a7a3ef53574a03
Carl F. Corneil <aecorn@gmail.com>
2023-12-05 13:05:31 +0100

    fixing for nox, module docstrings

--- src/klass/utility/classification.py
+++ src/klass/utility/classification.py
@@ -2,4 +2,5 @@ from ..classes.classification import KlassClassification
 
 
 def get_classification(classification_id: str) -> KlassClassification:
+    """Simple factory-function to create instances of KlassClassifications (classes are scary)."""
     return KlassClassification(classification_id)
