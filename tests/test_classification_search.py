f5328c364984e3852b0c2c505856e76e850dac88
Carl F. Corneil <aecorn@gmail.com>
2023-12-08 17:15:55 +0100

    gui testing

--- tests/test_classification_search.py
+++ tests/test_classification_search.py
@@ -1,3 +1,6 @@
+import klass
+
+
 def test_ClassificationSearchType_has_versions_content(
     klass_classification_search_success,
 ):
@@ -11,3 +14,11 @@ def test_ClassificationSearchType_has_str_repr(klass_classification_search_succe
     assert klass_classification_search_success.__repr__()
     assert len(klass_classification_search_success.__str__())
     assert len(klass_classification_search_success.__repr__())
+
+
+def test_gui_formatting(klass_classification_search_success):
+    result = klass.widgets.search_ipywidget.format_classification_text(
+        klass_classification_search_success
+    )
+    assert len(result)
+    assert isinstance(result, str)
