de7fa369b3f3584e65bc927303860a444bdc83af
Carl Corneil <cfc@ssb.no>
2026-02-20 13:51:16 +0100

    Update demo/correspondence.py

--- demo/correspondence.py
+++ demo/correspondence.py
@@ -7,7 +7,7 @@
 #       format_version: '1.3'
 #       jupytext_version: 1.19.1
 #   kernelspec:
-#     display_name: fake-stat-selvangivelse
+#     display_name: ssb-klass-python
 #     language: python
 #     name: fake-stat-selvangivelse
 # ---
